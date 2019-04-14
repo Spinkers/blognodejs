ee3f3b83310721e06c9c3abb3191c305598438c7
Lucas Lopes <lucasmouraolopes@gmail.com>
2019-04-14 10:50:42 -0300

    Subindo alteracoes significativas

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
-//const mongoose = require('mongoose');
+const admin = require('./routes/admin');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash());
+    //Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    });
     //Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -13,10 +30,25 @@ const app = express();
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars');
     //Mongoose
-        //Em breve
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/blogapp").then(() => {
+        console.log("Conectado ao mongo!");
+    }).catch((err) => {
+        console.log("Erro ao se conectar: " + err);
+    });
+    //Public
+    app.use(express.static(path.join(__dirname, "public")));
+    /*app.use((req, res, next) => {
+        console.log("Middleware!")
+        next();
+    });*/
 
 //Rotas
+app.get('/principal', (req, res) => {
+    res.send("Página principal");
+});
 
+app.use('/admin', admin);
 
 //Outros
 const PORT = 8081;
---
+++ routes/admin.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require("mongoose")
+require("../models/Categoria");
+const Categoria = mongoose.model("categorias");
+
+router.get('/', (req, res) => {
+    res.render('admin/index');
+});
+
+router.get('/posts', (req, res) => {
+    res.send("Página de posts");
+});
+
+router.get("/categorias", (req, res) => {
+    res.render("admin/categorias");
+});
+
+router.get('/categorias/add', (req, res) => {
+    res.render("admin/addcategorias")
+});
+
+router.post("/categorias/nova", (req, res) => {
+    
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"});
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"});
+    }
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria muito pequeno"})
+    }
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
+            res.redirect("/admin")
+        })
+    }
+
+});
+
+module.exports = router;
